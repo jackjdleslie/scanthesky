7b92d3ae4f2d0f6813113555c2bf6a6750fa4b95
Jack Leslie <jjdl@st-andrews.ac.uk>
2018-04-08 16:03:19 +0100

    Clean up

--- watch.js
+++ watch.js
@@ -4,7 +4,7 @@ let web3;
 var contract = require('truffle-contract');
 
 // Import our contract artifacts and turn them into usable abstractions.
-var artifacts = require('./build/contracts/ScanTheSky.json');
+var artifacts = require('../scanthesky/build/contracts/ScanTheSky.json');
 
 // ScanTheSky is our usable abstraction, which we'll use through the code below.
 var ScanTheSky = contract(artifacts);
@@ -18,31 +18,50 @@ var account;
 web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
 ScanTheSky.setProvider(web3.currentProvider);
 
-web3.eth.getAccounts(function(err, accs) {
-  if (err != null) {
-    alert("There was an error fetching your accounts.");
-    return;
-  }
-
-  if (accs.length == 0) {
-    alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-    return;
-  }
-
-  accounts = accs;
-  account = accounts[0];
-});
-
 ScanTheSky.deployed().then(instance => {
+
+  var confirmed = [];
+
   let hasBookedEvent = instance.LogHasBooked({}, {fromBlock: 0, toBlock: 'latest'});
   hasBookedEvent.get((error, logs) => {
     // we have the logs, now print them
-    logs.forEach(log => console.log(log.args));
+    console.log();
+    logs.forEach(log => {
+      console.log("Data for Skyscanner: ");
+      console.log("  Booking " + log.args._booking + " has been confirmed");
+      confirmed.push(log.args._booking);
+    });
+  });
+
+  let unconfirmedBookingEvent = instance.LogUnconfirmedBooking({}, {fromBlock: 0, toBlock: 'latest'});
+  unconfirmedBookingEvent.get((error, logs) => {
+    // we have the logs, now print them
+    console.log();
+    logs.forEach(log => {
+      if (!confirmed.includes(log.args._booking)) {
+        console.log("Data for Skyscanner: ");
+        console.log("  Booking " + log.args._booking + " has been submitted but not confirmed");
+      }
+    });
   });
 
   let timesBookedEvent = instance.LogTimesBooked({}, {fromBlock: 0, toBlock: 'latest'});
   timesBookedEvent.get((error, logs) => {
-    // we have the logs, now print them
-    logs.forEach(log => console.log(log.args));
+    var prev = logs[0];
+    var prevIndex = 0;
+
+    for (var i = 1; i < logs.length; i++) {
+      if (logs[i].args._count > prev.args._count) {
+        logs.splice(prevIndex, 1);
+      }
+      prev = logs[i];
+      prevIndex = i;
+    }
+    console.log();
+    logs.forEach(log => {
+      console.log("Data for Airline: " + log.args._airline);
+      console.log("  Skyscanner user " + log.args._user + " has booked with you " + log.args._count + " time(s)");
+    });
+    console.log();
   });
 });
