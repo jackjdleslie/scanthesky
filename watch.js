3b1cd49160c1a71abd684a0abf81621674fb8c6a
Jack Leslie <jjdl@st-andrews.ac.uk>
2018-04-08 22:45:04 +0100

    Tidy up of watch.js

--- watch.js
+++ watch.js
@@ -1,6 +1,5 @@
 // Import libraries we need.
 var Web3 = require('web3');
-let web3;
 var contract = require('truffle-contract');
 
 // Import our contract artifacts and turn them into usable abstractions.
@@ -9,22 +8,18 @@ var artifacts = require('../scanthesky/build/contracts/ScanTheSky.json');
 // ScanTheSky is our usable abstraction, which we'll use through the code below.
 var ScanTheSky = contract(artifacts);
 
-// The following code is simple to show off interacting with your contracts.
-// As your needs grow you will likely need to change its form and structure.
-// For application bootstrapping, check out window.addEventListener below.
-var accounts;
-var account;
-
-web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
+// app.js assumed truffle develop is the provider
+var web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
 ScanTheSky.setProvider(web3.currentProvider);
 
 ScanTheSky.deployed().then(instance => {
 
   var confirmed = [];
 
+  // get data from hasBooked events
   let hasBookedEvent = instance.LogHasBooked({}, {fromBlock: 0, toBlock: 'latest'});
+
   hasBookedEvent.get((error, logs) => {
-    // we have the logs, now print them
     console.log();
     logs.forEach(log => {
       console.log("Data for Skyscanner: ");
@@ -33,9 +28,10 @@ ScanTheSky.deployed().then(instance => {
     });
   });
 
+  // get data from unconfirmedBooking events
   let unconfirmedBookingEvent = instance.LogUnconfirmedBooking({}, {fromBlock: 0, toBlock: 'latest'});
+
   unconfirmedBookingEvent.get((error, logs) => {
-    // we have the logs, now print them
     console.log();
     logs.forEach(log => {
       if (!confirmed.includes(log.args._booking)) {
@@ -45,11 +41,14 @@ ScanTheSky.deployed().then(instance => {
     });
   });
 
+  // get data from timesBooked events
   let timesBookedEvent = instance.LogTimesBooked({}, {fromBlock: 0, toBlock: 'latest'});
+
   timesBookedEvent.get((error, logs) => {
     var prev = logs[0];
     var prevIndex = 0;
 
+    // filter logs to only use when user book count is highest
     for (var i = 1; i < logs.length; i++) {
       if (logs[i].args._count > prev.args._count) {
         logs.splice(prevIndex, 1);
@@ -57,6 +56,7 @@ ScanTheSky.deployed().then(instance => {
       prev = logs[i];
       prevIndex = i;
     }
+
     console.log();
     logs.forEach(log => {
       console.log("Data for Airline: " + log.args._airline);
@@ -64,4 +64,5 @@ ScanTheSky.deployed().then(instance => {
     });
     console.log();
   });
+
 });
